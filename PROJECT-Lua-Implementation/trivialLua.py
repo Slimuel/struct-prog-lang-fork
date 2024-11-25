1d2836e2af64b679679ee44bd4a1c5308b0970bf
Samuel Markus <sjmark4@gmail.com>
2024-11-25 15:43:02 -0500

    11-25 update

---
+++ PROJECT-Lua-Implementation/trivialLua.py
@@ -0,0 +1,25 @@
+# Trivial imports
+
+
+# Lupa import
+from lupa import LuaRuntime                      # import Lupa upon program startup
+lua = LuaRuntime(unpack_returned_tuples=True)    # Create variable for lua to run on
+
+
+"""
+TOKENIZER ADDITIONS
+- null
+"""
+
+
+
+"""
+PARSER ADDITIONS
+- null
+"""
+
+
+"""
+EVALUATOR ADDITIONS
+- null
+"""
